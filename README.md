5d6812f045144e119e1bf95cf69877664c383180
Ali Elahi <alielahi.1378@gmail.com>
2020-02-08 16:48:20 +0330

    README added

---
+++ README.md
@@ -0,0 +1,25 @@
+#URL_SHORTENER
+make links shorter
+
+####Run
+```
+dotnet run
+```
+####Test
+```
+dotnet test
+```
+####EndPoint
+For Post and shortener your URL :
+http://localhost:5000/urls
+```json
+{
+	"longUrl":"http://alibaba.ir"
+}
+```
+For Get and redirect :
+http://localhost:5000/redirect/"shortUrl"
+
+
+###Developer
+Ali Elahi
--- test/ConvertTest.cs
+++ test/ConvertTest.cs
@@ -1,10 +1,31 @@
 using System;
 using Xunit;
 using RA;
+using System.Text.RegularExpressions;
+
 namespace test
 {
     public class ConvertTest
     {
+        [Fact]
+        public void TestValidating() 
+        {
+            var body = new 
+            {
+                LongUrl = "http://alibaba.ir"
+            };
+            new RestAssured()
+             .Given()
+                .Name("long url validation")
+                .Header("Content-Type", "application/json")
+                .Body(body)
+            .When()
+                .Post("http://localhost:5000/urls")
+            .Then()
+                .TestBody("Validation", u => Regex.IsMatch(body.LongUrl, @"((www\.|(http|https|ftp|news|file)+\:\/\/)[_.a-z0-9-]+\.[a-z0-9\/_:@=.+?,##%&~-]*[^.|\'|\# |!|\(|?|,| |>|<|;|\)])"))
+                .TestStatus("Status code Test", s => s == 200)
+                .AssertAll();  
+        }
         
     }
 }
